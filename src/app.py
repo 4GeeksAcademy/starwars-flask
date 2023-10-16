d6a4b2a42127b1a3c81a718626d00c101157a3ff
Enmanuel Lobaton <138057675+elobaton@users.noreply.github.com>
2023-10-16 17:33:34 +0000

    flask

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +36,171 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#------------------------ RUTAS ------------------------#
+# Ruta para obtener usuarios (GET)
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    try:
+        all_users = User.query.all()
+        return [ user.serialize() for user in all_users ]
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 500
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Ruta para obtener un usuario por ID (GET)
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+        return user.serialize()
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}
 
-    return jsonify(response_body), 200
+# Ruta para obtener planetas (GET)
+@app.route('/planet', methods=['GET'])
+def get_all_planets():
+    try:
+        all_planets = Planet.query.all()
+        return [ planet.serialize() for planet in all_planets ]
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 500
+
+# Ruta para obtener un planeta por ID (GET)
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return {"Message": "Planet not found"}, 404
+
+        return planet.serialize()
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}
+
+# Ruta para obtener personajes (GET)
+@app.route('/character', methods=['GET'])
+def get_all_characters():
+    try:
+        all_characters = Character.query.all()
+        return [ character.serialize() for character in all_characters ]
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 500
+
+# Ruta para obtener un planeta por ID (GET)
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character_by_id(character_id):
+
+    try:
+        character = Character.query.get(character_id)
+        if character is None:
+            return {"Message": "Character not found"}, 404
+        return character.serialize()
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}
+
+# Ruta para obtener los favoritos de un usuario por ID (GET)
+@app.route('/user/<int:user_id>/favorite', methods=['GET'])
+def get_user_favorites(user_id):
+
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+
+        favorites = []
+        for favorite in user.favorites:
+            if favorite.planet is not None:
+                favorites.append(favorite.planet.serialize())
+            if favorite.character is not None:
+                favorites.append(favorite.character.serialize())
+
+        return jsonify({"favorites": favorites})
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}
+
+# Ruta para agregar un planeta como favorito de un usuario por ID (POST)
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_planet_favorite(user_id, planet_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+
+        planet = Planet.query.get(planet_id)
+        if planet is None:
+            return {"Message": "Planet not found"}, 404
+
+        favorite = Favorite(user=user, planet=planet)
+        db.session.add(favorite)
+        db.session.commit()
+
+        return {"Message": "Planet added to favorites successfully"}, 201
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 400
+
+# Ruta para agregar un personaje como favorito de un usuario por ID (POST)
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['POST'])
+def add_character_favorite(user_id, character_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+
+        character= Character.query.get(character_id)
+        if character is None:
+            return {"Message": "Character not found"}, 404
+
+        favorite = Favorite(user=user, character=character)
+        db.session.add(favorite)
+        db.session.commit()
+
+        return {"Message": "Character added to favorites successfully"}, 201
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 400
+
+# Ruta para eliminar un planeta como favorito de un usuario por ID (DELETE)
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet_favorite(user_id, planet_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+
+        favorite = Favorite.query.filter_by(
+            user_id=user_id, planet_id=planet_id
+        ).first()
+        if favorite is None:
+            return {"Message": "Favorite not found"}, 404
+
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return {"Message": "Favorite deleted successfully"}, 200
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 400
+
+# Ruta para eliminar un personaje como favorito de un usuario por ID (DELETE)
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_character_favorite(user_id, character_id):
+    try:
+        user = User.query.get(user_id)
+        if user is None:
+            return {"Message": "User not found"}, 404
+
+        favorite = Favorite.query.filter_by(
+            user_id=user_id, character_id=character_id
+        ).first()
+        if favorite is None:
+            return {"Message": "Favorite not found"}, 404
+
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return {"Message": "Favorite deleted successfully"}, 200
+    except ValueError as err:
+        return {"Message": "An error has occured: " + err}, 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -4,9 +4,18 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __init__(self, full_name, username, email, password):
+        self.full_name = full_name
+        self.username = username
+        self.email = email
+        self.password = password
+        self.is_active = True
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +23,78 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
+            "username": self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), nullable=False)
+    climate = db.Column(db.String(50), nullable=False)
+    terrain = db.Column(db.String(50), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, full_name, climate, terrain, population):
+        self.full_name = full_name
+        self.climate = climate
+        self.terrain = terrain
+        self.population = population
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.full_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50), nullable=False)
+    hair_color = db.Column(db.String(50), nullable=False)
+    occupation = db.Column(db.String(50), nullable=False)
+    history = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, full_name, hair_color, occupation, history):
+        self.full_name = full_name
+        self.hair_color = hair_color
+        self.occupation = occupation
+        self.history = history
+    
+    def __repr__(self):
+        return '<Character %r>' % self.full_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "hair_color": self.hair_color,
+            "occupation": self.occupation,
+            "history": self.history,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # User data
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship(User)
+
+    # Planet favorite
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship(Planet)
+
+    # Character favorite
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    character = db.relationship(Character)
+
+    def to_dict(self):
+        return {}
+
+User.favorites = db.relationship(Favorite, backref="user_favorites")
